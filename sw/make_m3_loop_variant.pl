038d57d0951d331dd4d22ece92985576ac5cca7b
qglint <qglint@outlook.com>
2026-07-14 19:45:02 +0800

    添加coe转换，更多的验证环境

--- sw/make_m3_loop_variant.pl
+++ sw/make_m3_loop_variant.pl
@@ -15,15 +15,15 @@ close $in;
 my @offsets = map { hex($_) } qw(
     a38 a84 a9c b94 bac c60 cb8 cd0 d9c db4 e74 e8c f58
 );
-my $old = 0x00100793;
 my $new = (($limit & 0xfff) << 20) | 0x00000793;
 
 for my $offset (@offsets) {
     die sprintf("image is too short for offset 0x%x\n", $offset)
         if $offset + 4 > length($image);
     my $word = unpack('V', substr($image, $offset, 4));
-    die sprintf("unexpected word 0x%08x at offset 0x%x\n", $word, $offset)
-        unless $word == $old;
+    die sprintf("unexpected instruction 0x%08x at loop offset 0x%x\n",
+                $word, $offset)
+        unless ($word & 0x000fffff) == 0x00000793;
     substr($image, $offset, 4, pack('V', $new));
 }
 
@@ -32,4 +32,3 @@ print {$out} $image;
 close $out;
 
 printf "patched %d M3 loop bounds to %d in %s\n", scalar(@offsets), $limit, $output;
-
